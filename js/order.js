70c64fe25fc6822e005e6621f06c31a7f853496f
RamiZregat <rami.zregat97@gmail.com>
2021-07-14 00:17:40 +0300

    order page

--- js/order.js
+++ js/order.js
@@ -1,50 +1,38 @@
 'use strict'
-
-let listArray=[];
-
+let listArray = [];
 function getData() {
-    let GettingData = localStorage.getItem('ShopCart')
-    let DataPrase = JSON.parse(GettingData)
-    if (DataPrase !== null) {
-        listArray = DataPrase
-    }
+  let GettingData = localStorage.getItem('ShopCart')
+  let DataPrase = JSON.parse(GettingData)
+  if (DataPrase !== null) {
+    listArray = DataPrase
   }
-  let Total
-  function getTotal(){
-    Total=localStorage.getItem('Total')
-  }
-
- getTotal()
-  getData()
-  
-  let render=document.getElementById('list');
-  let div=document.createElement('div');
-  div.id='productslist';
-  render.appendChild(div);
-
-  let ul=document.createElement('ul')
-  ul.id='itemsUl';
-  div.appendChild(ul)
-
-  for(let i=0;i<listArray.length;i++)
-  {
-
-      let li = document.createElement('li')
-      ul.appendChild(li)
-      let name=document.createElement('h2')
-      li.appendChild(name)
-      name.textContent=listArray[i][0]
-        let price=document.createElement('h3')
-        li.appendChild(price)
-        price.textContent=listArray[i][1]
-
-    let image=document.createElement('img')
-    image.setAttribute('src',`${listArray[i][2]}`)
-    li.appendChild(image)
-      
-  }
-  let totalprice=document.createElement('h1')
-  div.appendChild(totalprice)
-  totalprice.textContent=`Total: ${Total}`
-
-
+}
+let Total
+function getTotal() {
+  Total = localStorage.getItem('Total')
+}
+getTotal()
+getData()
+let render = document.getElementById('list');
+let div = document.createElement('div');
+div.id = 'productslist';
+render.appendChild(div);
+let ul = document.createElement('ul')
+ul.id = 'itemsUl';
+div.appendChild(ul)
+for (let i = 0; i < listArray.length; i++) {
+  let li = document.createElement('li')
+  ul.appendChild(li)
+  let image = document.createElement('img')
+  image.setAttribute('src', `${listArray[i][2]}`)
+  li.appendChild(image)
+  let name = document.createElement('h2')
+  li.appendChild(name)
+  name.textContent = listArray[i][0]
+  let price = document.createElement('h3')
+  li.appendChild(price)
+  price.textContent = `price : ${listArray[i][1]}`
+}
+let totalprice = document.createElement('h1')
+div.appendChild(totalprice)
+totalprice.textContent = `Total: $${Total}`
